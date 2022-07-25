50962e5373ed1a5f2eed7316d050173b475a835c
GANGIREDDY VISHNU VARDHAN REDDY <76578959+VVR1102001@users.noreply.github.com>
2022-07-25 19:25:57 +0530

    Update index.php

--- terms&conditions/index.php
+++ terms&conditions/index.php
@@ -1,1 +1,41 @@
-
+<!DOCTYPE html>
+<html lang="en-IN">
+  <head>
+    <title>vvrgiveaways - official website</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
+    <meta name="description" content="Special giveaway by vvr."/>
+    <meta name="keywords" content="free giveaway,campaign,holifestivalgiveaway,holi 2022,holi giveaway,holi special giveaway"/>
+    <meta charset="UTF-8" />
+    <link rel="apple-touch-icon" href="assets/giveaway.png" type="image/icon"/>
+    <link rel="icon" href="assets/logo.png" type="image/icon"/>
+    <meta name="author" content="Vishnu Vardhan Reddy"/>
+    <link rel="stylesheet" href="./aa.css"/>
+    
+    <link rel='dns-prefetch' href='//fonts.googleapis.com' />
+<link rel="canonical" href="https://www.vvrgiveaways.ml/" />
+<link rel="canonical" href="https://vvrgiveaways.ml/" />
+<link rel='shortlink' href="www.vvrgiveaways.ml" />
+<link rel="canonical" href="https://vvrgiveaways.ml" />
+<link rel="canonical" href="https://www.vvrgiveaways.ml" />
+    <script src="a1.js"></script>
+  </head>
+  <body>
+    <main id="tc">
+      <center>
+      <h2>Terms and Conditions</h2>
+    
+      <p>
+        1.Do not make scam to earn money.</p>
+        <p>2.Real and valid entries to be entered to</p> participate in giveaway.
+       <p>3.Currently only few users are eligible for giveaways.</p>
+       <p>4.Do not blame us if any of users makes wrong steps in giveaway.</p>
+       <p>5.Giveaway is invalid if vpn is used while participating in giveaway.</p>
+       <p>6.we are not responsible if your entry has mistakes (eg. Paytm number,name, telegram account name)</p>
+       <p>7.Enter paytm wallet number also make sure that you are entering valid number and your number</p>
+       <p>8.Every user can only participate once in every giveaway</p>
+       <p>9.Making scam is strictly prohibited such users no more entertained to participate in upcoming giveaways.</p>
+    </main>
+    </center>
+    </body>
+    </html>
